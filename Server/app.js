d4d79969b840593b1c7d1c12b3b893be1e958455
ThaoVu <thaovu.dev17@gmail.com>
2024-12-03 23:11:44 +0700

    update

--- Server/app.js
+++ Server/app.js
@@ -13,9 +13,9 @@ var comicRouter = require('./routes/comicRoutes');
 const nftRoutes = require('./routes/nftRounter');
 
 const adminRoutes = require('./routes/adminRounter');
-var AuthRounter = require('./routes/Auth_Rounters');
+var AuthRounter = require("./routes/Auth_Rounters");
 //session
-var session = require('express-session');
+var session = require('express-session')
 var app = express();
 
 // view engine setup
@@ -28,56 +28,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // Để xử lý các yêu cầu POST với JSON
 app.use(bodyParser.urlencoded({ extended: true })); // Để xử lý các form-urlencoded
 
+
 //session check
-app.use(
-    session({
-        secret: 'AAAAAAAAABBBBBBBBBCCCCCCCDDDDDDD',
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false },
-    })
-);
+app.use(session({
+  secret: 'AAAAAAAAABBBBBBBBBCCCCCCCDDDDDDD',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+
 
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+
 
 app.use('/wallet', walletRouter);
 app.use('/comics', comicRouter);
 app.use('/nft', nftRoutes);
 app.use('/admin', adminRoutes);
-app.use('/auth', AuthRounter);
-app.use('/', comicRouter); // Định nghĩa route cho '/'
+app.use("/auth", AuthRounter);
+
 
 websocketServices;
 
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
+  // render the error page
+  res.status(err.status || 500);
 
-    // link api sẽ có tiền tố /api ở đầu
-    if (req.originalUrl.indexOf('/api') == 0) {
-        // nếu = 0 thì có api
-        res.json({
-            status: err.status,
-            msg: err.message,
-        });
-    } else {
-        res.render('error');
+ // link api sẽ có tiền tố /api ở đầu
+ if(req.originalUrl.indexOf('/api') == 0) { // nếu = 0 thì có api
+  res.json(
+    {
+      status : err.status,
+      msg:  err.message
     }
-    res.render('error');
+    
+  )
+}
+else {
+  res.render('error');
+    }
+  res.render('error');
 });
 
 module.exports = app;
--- Server/routes/comicRoutes.js
+++ Server/routes/comicRoutes.js
@@ -1,41 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const comicController = require('../controller/comicController');
+const comicController = require("../controller/comicController");
 
 function requireAdmin(req, res, next) {
-    if (!req.session.userLogin) {
-        return res.redirect('/auth/signin');
-    }
-    if (req.session.userLogin.role === 1) {
-        return next();
-    }
-    if (req.session.userLogin.role === 2) {
-        return next();
-    }
-    console.log('Bạn không đủ quyền hạn');
-
-    return res.redirect('/comics');
+  if (!req.session.userLogin) {
+    return res.redirect("/auth/signin");
+  }
+  if (req.session.userLogin.role === 1) {
+    return next();
+  }
+  if (req.session.userLogin.role === 2) {
+    return next();
+  }
+  return res.redirect("/comics");
 }
 
-router.get('/', requireAdmin, comicController.getComics);
+router.get("/", requireAdmin, comicController.getComics);
 
-router.get('/search', requireAdmin, comicController.searchComics);
+router.get("/search", requireAdmin, comicController.searchComics);
 
 router.get(
-    '/readcomics/:idComics',
-    requireAdmin,
-    comicController.getReadComics
+  "/readcomics/:idComics",
+  requireAdmin,
+  comicController.getReadComics
 );
 
 router.get(
-    '/detailcomics/:slug',
-    requireAdmin,
-    comicController.getDetailComics
+  "/detailcomics/:slug",
+  requireAdmin,
+  comicController.getDetailComics
 );
-// Route để gửi đánh giá truyện (POST)
-router.post('/submitReview', requireAdmin, comicController.submitReview);
 
-// Route thêm vào danh sách yêu thích (POST)
-router.post('/addToFavorites', requireAdmin, comicController.addToFavorites);
-router.get('/stats', requireAdmin, comicController.getStats);
 module.exports = router;
